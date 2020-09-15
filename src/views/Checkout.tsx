e3c5b6aba34aa1df0c310d8c36dd5bea8ca1516c
Oyedeji Peace <oyedejipeace@gmail.com>
2020-09-15 15:09:15 +0100

    fix checkout issues

--- src/views/Checkout.tsx
+++ src/views/Checkout.tsx
@@ -8,7 +8,9 @@ import Row from 'react-bootstrap/Row'
 import Wrapper from '../components/Wrapper'
 import { addReservation } from '../redux/actions/reservations'
 import PaymentForm from '../components/PaymentForm'
+import { IRootState } from '../redux/reducers'
 import { INewReservation, IStripeCharge } from '../types/reservationsTypes'
+import { IUserState } from '../types/usersTypes'
 
 const stripeKey = process.env.REACT_APP_STRIPE_KEY
 const stripePromise = loadStripe(stripeKey || '')
@@ -19,13 +21,25 @@ interface IProps extends RouteComponentProps {
     reservation: INewReservation,
     stripeToken: string
   ) => Promise<IStripeCharge | undefined>
+  userState: IUserState
 }
 
-const Checkout: FC<IProps> = ({ history, reservation, addReservation }): ReactElement => {
+const Checkout: FC<IProps> = ({
+  history,
+  reservation,
+  addReservation,
+  userState
+}): ReactElement => {
   if (reservation === null) history.push('/reservation')
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
+
+  useEffect(() => {
+    if (!userState.isLoggedIn) {
+      history.push('/')
+    }
+  }, [userState.isLoggedIn, history])
 
   return (
     <div className="checkout-form">
@@ -44,7 +58,7 @@ const Checkout: FC<IProps> = ({ history, reservation, addReservation }): ReactEl
   )
 }
 
-const mapStateToProps = () => ({})
+const mapStateToProps = (state: IRootState) => ({ userState: state.userState })
 const mapDispatchToProps = { addReservation }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout)
